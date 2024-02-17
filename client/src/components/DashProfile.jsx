1cc7537758375a179876bd2959b51cdff9e072b8
BoatBoat <bbdev.mm@gmail.com>
2024-02-17 13:30:56 +0630

    add create a post page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -18,6 +18,7 @@ deleteUserFailure,
 signoutSuccess } from '../redux/user/userSlice';
 
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
   const { currentUser, error, loading } = useSelector((state) => state.user);
@@ -217,10 +218,25 @@ const handleSignout = async () => {
 
 <TextInput type='password' id='password' placeholder='password' onChange={handleChange}
          />
-         <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-          Update
+         <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}>
+          
+          {loading ? 'Loading...' : 'Update'}
 
          </Button>
+
+         {
+          currentUser.isAdmin && (
+            <Link to={'/create-post'}>
+            <Button 
+            type='button'
+            gradientDuoTone='purpleToPink'
+            className='w-full'
+            >
+              Create a post
+            </Button>
+            </Link>
+          )
+         }
 
         </form>
         <div className='text-red-500 flex justify-between mt-5'>
